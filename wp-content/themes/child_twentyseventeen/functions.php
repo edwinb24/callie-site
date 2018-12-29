b24da194347d10f7f92e9c8111cc323f158f1eee
edwinb24 <edwin_b24@hotmail.com>
2018-12-28 18:39:23 -0600

    Added Bootstrap and Font Awesome

--- wp-content/themes/child_twentyseventeen/functions.php
+++ wp-content/themes/child_twentyseventeen/functions.php
@@ -2,6 +2,6 @@
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-sass', get_stylesheet_directory_uri() . '/build/css/child-styles.css', array(), '1.0', 'all' );
+    wp_enqueue_style( 'child-sass', get_stylesheet_directory_uri() . '/build/css/styles.css', array(), '1.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
