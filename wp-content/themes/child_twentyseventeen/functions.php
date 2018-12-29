176be00da31971c759b19ad8ca94d711690a7a0e
edwinb24 <edwin_b24@hotmail.com>
2018-12-29 11:40:02 -0600

    Setup minified versions of css and javascript and simplify compiler
    Created min versions of css and js and simplify packaging to run with the simple command gulp

--- wp-content/themes/child_twentyseventeen/functions.php
+++ wp-content/themes/child_twentyseventeen/functions.php
@@ -2,6 +2,11 @@
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-sass', get_stylesheet_directory_uri() . '/build/css/styles.css', array(), '1.0', 'all' );
+    wp_enqueue_style( 'child-styles', get_stylesheet_directory_uri() . '/assets/css/styles.css', array(), '1.0', 'all' );
 }
+function my_theme_enqueue_script() {
+	wp_enqueue_script( 'child-script', get_stylesheet_directory_uri() . '/assets/js/libs.js', array(), '1.0', 'all'  );
+}
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_script' );
--- wp-content/themes/child_twentyseventeen/gulpFile.js
+++ wp-content/themes/child_twentyseventeen/gulpFile.js
@@ -1,13 +1,33 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    watch = require('gulp-watch');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var cssMin = require('gulp-css');
 
+//'gulp sass' command
 gulp.task('sass', function(){
     gulp.src('./assets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./build/css'));
+        .pipe(gulp.dest('./assets/css/uncompress'));
 });
 
-gulp.task('default', ['sass'], function() {
-    return gulp.watch('./assets/sass/**/*.scss', ['sass']);
-});
+gulp.task('css',function() {
+  gulp.src([
+            "./assets/css/uncompress/styles.css"
+        ])
+        .pipe(concat('styles.css'))
+        .pipe(cssMin())
+        .pipe(gulp.dest('./assets/css'));
+})
+
+gulp.task('scripts', function() {
+  gulp.src([
+          "./assets/js/scripts/main.js"
+        ])
+        .pipe(concat('libs.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest("./assets/js"));
+})
+
+gulp.task('default', ['sass','css','scripts']);
